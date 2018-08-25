97ab11e2d9c95cf0b375f36db3da1345c11656f7
Neeraj Kumar <kneeraj508@gmail.com>
2018-08-25 20:56:11 +0530

    Update to spring webflux and moved from maven to gradle.

--- src/main/java/com/neeraj/orion/repository/ListRepository.java
+++
@@ -1,9 +0,0 @@
-package com.neeraj.orion.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.neeraj.orion.model.ItemOnList;
-
-public interface ListRepository extends CrudRepository<ItemOnList, Long> {
-
-}
